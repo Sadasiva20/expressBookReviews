05708522018fcc43805c7a37fbedf48e9ccb271b
Sadasiva20 <sssank2@gmail.com>
2025-02-11 23:00:11 -0500

    Updates

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,24 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if(req.session.authorization){
+    let token = req.session.authorization['access'];
+    jwt.verify(token, "access_token", (err,customer) =>{
+
+        if(!err){
+            req.customer= customer;
+            next();
+          
+        }else{
+            res.status(403).json({message:"Unauthorized"});
+            
+
+        }
+    });
+} else{
+    return res.status(403).json({message:"Customer is not authorized."});
+}
+
 });
  
 const PORT =5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,62 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let username = users.find(user => user.username === username);
+
+if(username){
+   return true;
+}
+return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+
+let authenticated_users = users.filter((users) => {
+
+    return (users.username === username && users.password === password);
+});
+
+if(authenticated_users.length > 0){
+
+    return true;
+}
+return false;
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here 
+  let username = req.body.username;
+  let password = req.body.password;
+  if(authenticatedUser(username, password)){
+    let token = jwt.sign({
+        data:password
+    }) 
+    req.session.authorization = {accesstoken, username}
+    return res.status(200).json({message: "Login successful"});
+  }else{
+
+    return res.status(403).json({message: "Invalid credentials"});
+  }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let review = req.body.review;
+  if(books){
+
+    books[isbn]["reviews"].push(review);
+    return res.status(200).json({message: "Review added successfully"});
+  }
+   else{
+    return res.status(400).json({message: "Review was not added."});
+   }
+
 });
 
 module.exports.authenticated = regd_users;
