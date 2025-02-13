753ed52d46ab505342d346a3849cbb635298a113
Sadasiva20 <99778144+Sadasiva20@users.noreply.github.com>
2025-02-13 12:26:59 -0500

    Update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,7 +7,7 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
-username = users.filter((users));
+username = users.find(user => user.username === username);
 if(username.length >0 ){
    return true;
 }
