390ed5ee2e92e71819448de94d2887ff83ed6f98
Sadasiva20 <99778144+Sadasiva20@users.noreply.github.com>
2025-02-12 22:31:14 -0500

    Update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,19 +29,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
 
   const isbn = req.params.isbn;
-  if(books[isbn]===isbn){
       res.send(JSON.stringify(books[isbn]));
-  }
-
 
 });
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  if (books[author]===author){
         res.send(JSON.stringify(books[author]));
-  }
+  
 });
 
 
@@ -49,18 +45,14 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
-   if(books[title]===title){
     res.send(JSON.stringify(books[title]));
-   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
    const review = req.params.review;
-   if (books[review]===review){
         res.send(JSON.stringify(books[review]));
-   }
 });
 
 module.exports.general = public_users;
