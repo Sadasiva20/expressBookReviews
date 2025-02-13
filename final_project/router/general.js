b353f7dd437a122fcac150cfd1368773781370bb
Sadasiva20 <99778144+Sadasiva20@users.noreply.github.com>
2025-02-12 19:06:07 -0500

    Update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,47 +11,56 @@ public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
   if(isValid(username)){
-    users.push({username: username, password: password});
-    return res.status(200).json({message: "User successfully registered"});
+    users.push({username,password});
+        return  res.status(200).json({message: "User successfully registered"});
   }
-  return res.status(403).json({message: "User cannot be registered."});
+      return  res.status(401).json({message: "User cannot be registered."});
 
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-   res.send(JSON.stringify(books,null,10));
+      res.send(JSON.stringify(books,null,10));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+
   const isbn = req.params.isbn;
-   res.send(books[isbn]);
- });
-  
+  if(books[isbn]===isbn){
+      res.send(JSON.stringify(books[isbn]));
+  }
+
+
+});
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  res.send(books[author]);
- 
+  if (books[author]===author){
+        res.send(JSON.stringify(books[author]));
+  }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
-
-   res.send(books[title]);
+   if(books[title]===title){
+    res.send(JSON.stringify(books[title]));
+   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-   const isbn = req.params.isbn;
-   res.send(books[isbn]["reviews"]);
+   const review = req.params.review;
+   if (books[review]===review){
+        res.send(JSON.stringify(books[review]));
+   }
 });
 
 module.exports.general = public_users;
